e7984ba50283773ee0515476b09ce62db15173a9
lizy <1064969614@qq.com>
2018-08-06 22:15:41 +0800

    binary search

---
+++ src/main/java/com/lizy/algorithms/algorithms4thEdition/chapter_one_fundamentals/BinarySearch.java
@@ -0,0 +1,21 @@
+package com.lizy.algorithms.algorithms4thEdition.chapter_one_fundamentals;
+
+public class BinarySearch {
+    private BinarySearch(){}
+
+    public static int indexOf(int[] a, int key) {
+        int lo = 0;
+        int hi = a.length - 1;
+        while (lo <= hi) {
+            int mid = lo+(hi-lo)/2;
+            if(key<a[mid])
+                hi = mid-1;
+            else if (key > a[mid]) {
+                lo = mid+1;
+            }else
+                return mid;
+        }
+        return -1;
+    }
+
+}
