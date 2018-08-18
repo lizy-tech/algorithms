4e31f3730fc2aeae2f00b0968fdc310fea5415ed
lizhiyong <1064969614@qq.com>
2018-08-18 23:42:42 +0800

    sort

---
+++ src/main/java/com/lizy/algorithms/algorithms4thEdition/ch1/bagsQueuesStacks/Queue.java
@@ -0,0 +1,107 @@
+package com.lizy.algorithms.algorithms4thEdition.ch1.bagsQueuesStacks;
+
+import com.lizy.algorithms.util.StdIn;
+import com.lizy.algorithms.util.StdOut;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class Queue<Item> implements Iterable<Item> {
+    private Node<Item> first;
+    private Node<Item> last;
+    private int n;
+
+    private static class Node<Item>{
+        private Item item;
+        private Node<Item> next;
+    }
+
+    public Queue(){
+        first = null;
+        last = null;
+        n = 0;
+    }
+
+    public boolean isEmpty(){
+        return first == null;
+    }
+
+    public int size() {
+        return n;
+    }
+
+    public Item peek(){
+        if(isEmpty())
+            throw new NoSuchElementException("Queue underflow");
+        return first.item;
+    }
+
+    public void enqueue(Item item) {
+        Node<Item> oldlast = last;
+        last = new Node<Item>();
+        last.item = item;
+        last.next = null;
+        if(isEmpty())
+            first = last;
+        else
+            oldlast.next = last;
+        n++;
+    }
+
+    public Item dequeue() {
+        if (isEmpty())
+            throw new NoSuchElementException("Queue underflow");
+        Item item = first.item;
+        first = first.next;
+        n--;
+        if (isEmpty())
+            last = null;   // to avoid loitering
+        return item;
+    }
+
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        for (Item item : this) {
+            s.append(item);
+            s.append(' ');
+        }
+        return s.toString();
+    }
+
+    public Iterator<Item> iterator()  {
+        return new ListIterator<Item>(first);
+    }
+
+    private class ListIterator<Item> implements Iterator<Item> {
+        private Node<Item> current;
+
+        public ListIterator(Node<Item> first) {
+            current = first;
+        }
+
+        public boolean hasNext()  { return current != null;                     }
+        public void remove()      { throw new UnsupportedOperationException();  }
+
+        public Item next() {
+            if (!hasNext()) throw new NoSuchElementException();
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+    }
+
+    public static void main(String[] args) {
+        Queue<String> queue = new Queue<String>();
+        queue.enqueue("aaa");
+        StdOut.println(queue);
+
+        while (!StdIn.isEmpty()) {
+            String item = StdIn.readString();
+            if (!item.equals("-"))
+                queue.enqueue(item);
+            else if (!queue.isEmpty())
+                StdOut.print(queue.dequeue() + " ");
+        }
+        StdOut.println("(" + queue.size() + " left on queue)");
+    }
+}
