905d278b53af8c11e1aa9783c26f692a9be8e497
lizy <lizy@19pay.com.cn>
2018-10-09 17:17:13 +0800

    leetcode-41

---
+++ src/main/java/com/lizy/algorithms/leetcode/LeetCode_41.java
@@ -0,0 +1,31 @@
+package com.lizy.algorithms.leetcode;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author lizy@19pay.com.cn
+ * @date 2018-10-8 20:46
+ */
+public class LeetCode_41 {
+    public static int firstMissingPositive(int[] nums) {
+        int len = nums.length;
+        Set<Integer> set = new HashSet<>();
+        int count = 1;
+        for (int i = 0; i < len; i++) {
+            set.add(nums[i]);
+            while (set.contains(count)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {3,4,-1,1};
+        int res = firstMissingPositive(nums);
+        System.out.println(res);
+    }
+}
